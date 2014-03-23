d9aaab33346a803d7f9d1727bef034da771e0236
Kate <kate9000@gmail.com>
2014-03-23 13:07:00 -0700

    changes autonomous claw angle

--- AutonomousModes/RightLeftAuton.cpp
+++ AutonomousModes/RightLeftAuton.cpp
@@ -25,7 +25,7 @@ void Robot::RightLeftAuton() {
 	Wait (0.5);
 	robotDrive->drive(-0.1, 0);
 	Wait (.25);
-	claw->SetAngle(108.0);
+	claw->SetAngle(115.0);
 	Wait (0.5);
 
 	/* "&& robotDrive->getRightDist() > -5.0" ensures robot doesn't drive
